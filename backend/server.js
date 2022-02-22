e27d1f5849f82e32a282b30d4412cadccb66839e
Șerban Alexandru Jonny <alexjohnny6@gmail.com>
2022-02-22 22:55:48 +0200

    user routes, model, controller; jwt token, protect middleware

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/notes', require('./routes/notesRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
 app.use(errorHandler);
 
 app.listen(port, () => {
