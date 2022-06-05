48ee2bdbdf7f774805bd8dd57bdc3664f7dd240a
Șerban Alexandru Jonny <alexjohnny6@gmail.com>
2022-06-05 10:48:47 +0300

    added anc config Reactotron; fix on authSlice extraReducers;

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
+import Reactotron from '../ReactotronConfig';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
   },
+  enhancers: [Reactotron.createEnhancer()],
 });
